b71485b8ec08074291138bbb4021d663181f19b5
Mr-Bossman <mr.Bossman@gmail.com>
2020-07-29 22:27:09 -0400

    t

--- test.c
+++ test.c
@@ -2,35 +2,59 @@
 #include <string.h>
 #include <stdint.h>
 #include <wmmintrin.h>  //for intrinsics for AES-NI
+#include "immintrin.h"
 #include <stdalign.h>
-#include "aes.h"
+#define MIX4(s0, s1, s2, s3) \
+  tmp  = _mm_unpacklo_epi32(s0, s1); \
+  s0 = _mm_unpackhi_epi32(s0, s1); \
+  s1 = _mm_unpacklo_epi32(s2, s3); \
+  s2 = _mm_unpackhi_epi32(s2, s3); \
+  s3 = _mm_unpacklo_epi32(s0, s2); \
+  s0 = _mm_unpackhi_epi32(s0, s2); \
+  s2 = _mm_unpackhi_epi32(s1, tmp); \
+  s1 = _mm_unpacklo_epi32(s1, tmp);
 
 
 
 
-static void phex(uint8_t* str)
+static void phex(uint32_t* str,uint8_t len)
 {
-    uint8_t len = 16;
 
     unsigned char i;
     for (i = 0; i < len; ++i)
-        printf("%.2x", str[i]);
+        printf("%lld, ", str[i]);
     printf("\n");
 }
-int main(void)
-{
 
+static void phex(uint8_t* str)
+{
+    uint8_t len = 64;
+
+    unsigned char i;
+    for (i = 0; i < len; ++i)
+        printf("%lx, ", str[i]);
+    printf("\n");
+}
+int main() {
 	alignas(64) uint8_t a[64] = {0x6b, 0x2e, 0xe9, 0x73, 0xc1, 0x40, 0x3d, 0x93, 0xbe, 0x9f, 0x7e, 0x17, 0xe2, 0x96, 0x11, 0x2a};
 	alignas(64) uint8_t b[64] = {0x6b, 0x2e, 0xe9, 0x73, 0xc1, 0x40, 0x3d, 0x93, 0xbe, 0x9f, 0x7e, 0x17, 0xe2, 0x96, 0x11, 0x2a};
+	alignas(64) uint8_t c[64] = {0x6b, 0x2e, 0xe9, 0x73, 0xc1, 0x40, 0x3d, 0x93, 0xbe, 0x9f, 0x7e, 0x17, 0xe2, 0x96, 0x11, 0x2a};
 	alignas(64) uint8_t key[64] = {0x6b, 0x2e, 0xe9, 0x73, 0xc1, 0x40, 0x3d, 0x93, 0xbe, 0x9f, 0x7e, 0x17, 0xe2, 0x96, 0x11, 0x2a};
+	load_constants();
+ __m512i s = LOAD (b);
+ __m512i i = LOAD (key);
+
 	__m128i k = _mm_loadu_si128((__m128i *) key);
 	__m128i test = _mm_loadu_si128((__m128i *) b);
         __m128i m =_mm_aesenc_si128    (test, k);
    	 _mm_storeu_si128((__m128i *) a,m);
-	aes((_512_state*)b,(_512_key*)key);
-	phex(b);
-	phex(a);
-	
-    return 0;
+	aes((_512_state*)c,(_512_key*)key);
+  s = aes2(s,i);
+  STORE(b,s);
+  phex(b,64);
+  phex(a,64);
+  phex(c, 64);
+	//haraka512(b,a);
+//phex(b);
 }
 
